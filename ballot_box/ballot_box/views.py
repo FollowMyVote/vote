48657a31364c8e176a09d368278a49d2d26a26e6
Chris Mullins <cmullins@iddnet.com>
2014-11-11 17:17:20 -0500

    work on ballot box layout

--- ballot_box/ballot_box/views.py
+++ ballot_box/ballot_box/views.py
@@ -14,6 +14,15 @@ from ballot_box.modules.helpers import log, alert, date_str_to_iso
 def home():
     """Get Home Page"""
     log().debug("Render Page: Home")
+    return redirect('/ballot-box')
     return render_template('index.html',
         title='Home Page',)
 
+
+
+@app.route('/ballot-box')
+def ballot_box():
+    """Get Balot Box Page"""
+    log().debug("Render Page: ballot-box")
+    return render_template('ballot-box.html',
+        title='Ballot Box',)
